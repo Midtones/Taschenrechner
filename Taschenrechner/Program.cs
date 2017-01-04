c24690dc8319b369a86659f95d323715b34705f7
Tommy <Tommy@DESKTOP-OE3MTNG>
2017-01-04 16:08:28 +0100

    Projektdateien hinzufügen.

---
+++ Taschenrechner/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Anforderung -> Addieren: Benutzer gibt zwei Zahlen ein zum Addieren.
+
+            Console.Write("Bitte gib die erste Zahl ein: "); // gibt Text aus als Anweisung für den Nutzer.
+            string ersteZahl = Console.ReadLine();
+
+            Console.Write("Bitte gib die zweite Zahl ein: "); // gibt Text aus als Anweisung für den Nutzer.
+            string zweiteZahl = Console.ReadLine();
+
+            // Berechnung wird ausgeführt
+            int ergebnis = Convert.ToInt32(ersteZahl) + int.Parse(zweiteZahl); // Ergebnis berechnet die beiden Zahlen. int.Parse konvertiert den Text in eine Zahl.
+            
+            // Ausgabe
+            Console.WriteLine("Das Ergebnis ist {0}", ergebnis);
+            Console.ReadKey();
+        }
+    }
+}
