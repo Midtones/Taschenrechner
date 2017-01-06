f7e0b51d7f02b1cc53e8a18196b9e84b066bcaf8
Midtones <thomas.meyer@midtones-design.de>
2017-01-06 18:15:38 +0100

    Klasse RechnerModel erstellt und berrechnungs Methoden eingefügt.

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -22,7 +22,8 @@ namespace Taschenrechner
             string operation = HoleOperator("gib die auszuführende Operation ein (+ - / *): ");
 
             // Berrechnung wird ausgeführt und Ausgegeben. Gelöst mit Switch
-            float ausgabeErgebnis = BerrechnungMitSwitchCase(operation, zahl1, zahl2);
+            RechnerModel model = new RechnerModel();
+            float ausgabeErgebnis = model.BerrechneMitSwitchCase(operation, zahl1, zahl2);
             Console.WriteLine("Das Ergebnis {1} {0} {2} = {3}",operation ,zahl1, zahl2, ausgabeErgebnis);
 
             // Kommandozeile wartet auf eine Benutzereingabe
@@ -55,135 +56,6 @@ namespace Taschenrechner
             string wert = Console.ReadLine(); // Die Konsoleneingabe wirt in die Variable wert gespeichert.
 
             return wert; //Ausgabe des eingegbenen Wertes.
-        }
-
-        /// <summary>
-        /// Addiert zweit Werte zusammen.
-        /// </summary>
-        /// <param name="wert1"></param>
-        /// <param name="wert2"></param>
-        /// <returns></returns>
-        static float Addieren(float wert1, float wert2)
-        {
-            // Addiert wert1 mit wert2
-            float summe = wert1 + wert2;
-
-            // Gibt das Ergebnis aus der Methode zurück
-            return summe;
-        }
-
-        /// <summary>
-        /// Subtrahiert zweit Wert von einander.
-        /// </summary>
-        /// <param name="wert1"></param>
-        /// <param name="wert2"></param>
-        /// <returns></returns>
-        static float Subtrahieren(float wert1, float wert2)
-        {
-            // Subtrahiere wert1 mit wert2
-            float differenz = wert1 - wert2;
-
-            // Gibt das Ergebnis der Subtraktion zurück
-            return differenz;
-        }
-
-        /// <summary>
-        /// Dividiert zweit Wert.
-        /// </summary>
-        /// <param name="wert1"></param>
-        /// <param name="wert2"></param>
-        /// <returns></returns>
-        static float Division(float wert1, float wert2)
-        {
-            // Subtrahiere wert1 mit wert2
-            float differenz = wert1 / wert2;
-
-            // Gibt das Ergebnis der Subtraktion zurück
-            return differenz;
-        }
-
-        /// <summary>
-        /// Multipliziert zweit Wert.
-        /// </summary>
-        /// <param name="wert1"></param>
-        /// <param name="wert2"></param>
-        /// <returns></returns>
-        static float Multiplikation(float wert1, float wert2)
-        {
-            // Subtrahiere wert1 mit wert2
-            float produkt = wert1 * wert2;
-
-            // Gibt das Ergebnis der Subtraktion zurück
-            return produkt;
-        }
-
-        /// <summary>
-        /// Berrechnet zwei Zahlen mit ausgewählter operation. Note: Methode mit SwitchCase
-        /// </summary>
-        /// <param name="operation"></param>
-        /// <param name="zahl1"></param>
-        /// <param name="zahl2"></param>
-        static float BerrechnungMitSwitchCase(string operation, float zahl1, float zahl2)
-        {
-            float ergebnis = 0F; // Variable muss ausserhalb der switch Anweisung stehen!
-
-            switch (operation)
-            {
-                case "+":
-                    ergebnis = Addieren(zahl1, zahl2); // Ergebnis berechnet die beiden Zahlen.
-                    break;
-
-                case "-":
-                    ergebnis = Subtrahieren(zahl1, zahl2); // Ergebnis berechnet die beiden Zahlen.
-                    break;
-
-                case "/":
-                    ergebnis = Division(zahl1, zahl2); // Ergebnis berechnet die beiden Zahlen.
-                    break;
-
-                case "*":
-                    ergebnis = Multiplikation(zahl1, zahl2); // Ergebnis berechnet die beiden Zahlen.
-                    break;
-
-                default:
-                    Console.WriteLine("Dein Operator war ungültig! Du hast {0} eingegeben damit kann ich nicht rechnen!", operation);
-                    break;
-            }
-            return ergebnis;
-        }
-
-        /// <summary>
-        /// Berrechnet zwei Zahlen mit ausgewählter operation. Note: Methode mit else if
-        /// </summary>
-        /// <param name="operation"></param>
-        /// <param name="zahl1"></param>
-        /// <param name="zahl2"></param>
-        /// <returns></returns>
-        static float BerrechnungMitIfElse(string operation, float zahl1, float zahl2)
-        {
-            float ergebnis = 0F;
-            // Berechnung wird ausgeführt gelöst mit if und else if
-            if (operation == "+")
-            {
-                ergebnis = Addieren(zahl1, zahl2); // Ergebnis berechnet die beiden Zahlen.
-            }
-            else if (operation == "-")
-            {
-                ergebnis = Subtrahieren(zahl1, zahl2); // Ergebnis berechnet die beiden Zahlen.
-            }
-            else if (operation == "/")
-            {
-                ergebnis = Division(zahl1, zahl2); // Ergebnis berechnet die beiden Zahlen.
-            }
-            else if (operation == "*")
-            {
-                ergebnis = Multiplikation(zahl1, zahl2); // Ergebnis berechnet die beiden Zahlen.
-            }
-            else
-            {
-                Console.WriteLine("Dein Operator war ungültig!");
-            }
-            return ergebnis;
         }
 
         /// <summary>
---
+++ Taschenrechner/RechnerModel.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner
+{
+    class RechnerModel
+    {
+
+        /// <summary>
+        /// Berrechnet zwei Zahlen mit ausgewählter operation. Note: Methode mit SwitchCase
+        /// </summary>
+        /// <param name="operation"></param>
+        /// <param name="zahl1"></param>
+        /// <param name="zahl2"></param>
+        public float BerrechneMitSwitchCase(string operation, float zahl1, float zahl2)
+        {
+            float ergebnis = 0F; // Variable muss ausserhalb der switch Anweisung stehen!
+
+            switch (operation)
+            {
+                case "+":
+                    ergebnis = Addieren(zahl1, zahl2); // Ergebnis berechnet die beiden Zahlen.
+                    break;
+
+                case "-":
+                    ergebnis = Subtrahieren(zahl1, zahl2); // Ergebnis berechnet die beiden Zahlen.
+                    break;
+
+                case "/":
+                    ergebnis = Division(zahl1, zahl2); // Ergebnis berechnet die beiden Zahlen.
+                    break;
+
+                case "*":
+                    ergebnis = Multiplikation(zahl1, zahl2); // Ergebnis berechnet die beiden Zahlen.
+                    break;
+
+                default:
+                    Console.WriteLine("Dein Operator war ungültig! Du hast {0} eingegeben damit kann ich nicht rechnen!", operation);
+                    break;
+            }
+            return ergebnis;
+        }
+
+        /// <summary>
+        /// Addiert zweit Werte zusammen.
+        /// </summary>
+        /// <param name="wert1"></param>
+        /// <param name="wert2"></param>
+        /// <returns></returns>
+        private float Addieren(float wert1, float wert2)
+        {
+            // Addiert wert1 mit wert2
+            float summe = wert1 + wert2;
+
+            // Gibt das Ergebnis aus der Methode zurück
+            return summe;
+        }
+
+        /// <summary>
+        /// Subtrahiert zweit Wert von einander.
+        /// </summary>
+        /// <param name="wert1"></param>
+        /// <param name="wert2"></param>
+        /// <returns></returns>
+        private float Subtrahieren(float wert1, float wert2)
+        {
+            // Subtrahiere wert1 mit wert2
+            float differenz = wert1 - wert2;
+
+            // Gibt das Ergebnis der Subtraktion zurück
+            return differenz;
+        }
+
+        /// <summary>
+        /// Dividiert zweit Wert.
+        /// </summary>
+        /// <param name="wert1"></param>
+        /// <param name="wert2"></param>
+        /// <returns></returns>
+        private float Division(float wert1, float wert2)
+        {
+            // Subtrahiere wert1 mit wert2
+            float differenz = wert1 / wert2;
+
+            // Gibt das Ergebnis der Subtraktion zurück
+            return differenz;
+        }
+
+        /// <summary>
+        /// Multipliziert zweit Wert.
+        /// </summary>
+        /// <param name="wert1"></param>
+        /// <param name="wert2"></param>
+        /// <returns></returns>
+        private float Multiplikation(float wert1, float wert2)
+        {
+            // Subtrahiere wert1 mit wert2
+            float produkt = wert1 * wert2;
+
+            // Gibt das Ergebnis der Subtraktion zurück
+            return produkt;
+        }
+
+        /// <summary>
+        /// Berrechnet zwei Zahlen mit ausgewählter operation. Note: Methode mit else if
+        /// </summary>
+        /// <param name="operation"></param>
+        /// <param name="zahl1"></param>
+        /// <param name="zahl2"></param>
+        /// <returns></returns>
+        public float BerrechneMitIfElse(string operation, float zahl1, float zahl2)
+        {
+            float ergebnis = 0F;
+            // Berechnung wird ausgeführt gelöst mit if und else if
+            if (operation == "+")
+            {
+                ergebnis = Addieren(zahl1, zahl2); // Ergebnis berechnet die beiden Zahlen.
+            }
+            else if (operation == "-")
+            {
+                ergebnis = Subtrahieren(zahl1, zahl2); // Ergebnis berechnet die beiden Zahlen.
+            }
+            else if (operation == "/")
+            {
+                ergebnis = Division(zahl1, zahl2); // Ergebnis berechnet die beiden Zahlen.
+            }
+            else if (operation == "*")
+            {
+                ergebnis = Multiplikation(zahl1, zahl2); // Ergebnis berechnet die beiden Zahlen.
+            }
+            else
+            {
+                Console.WriteLine("Dein Operator war ungültig!");
+            }
+            return ergebnis;
+        }
+    }
+}
